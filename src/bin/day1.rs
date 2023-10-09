d802145129c004c75ca09d58d405c7dd2b0a4194
btee <b.kramarski@gmail.com>
2023-10-09 21:23:53 +0200

    Updated day1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,25 +1,28 @@
 use std::io;
 
 fn main() {
-    let list = read_ints();
     let mut increase_counter = 0;
-    let mut previous: Option<i64> = None;
-
-    for l in list {
-        if let Some(prev) = previous {
-            if prev < l {
-                increase_counter+=1;
+    const FIRST_TAKEN: usize = 0;
+    let list = read_ints();
+    if let Some(first) = list.get(FIRST_TAKEN) {
+        let mut previous = first;
+        for l in list.iter().skip(FIRST_TAKEN + 1) {
+            if previous < l {
+                increase_counter += 1;
             }
+            previous = l;
         }
-        previous = Some(l);
     }
 
     println!("{}", increase_counter)
 }
 
-fn read_ints() -> Vec<i64>{
+fn read_ints() -> Vec<i64> {
     if let Ok(lines) = read_lines() {
-        lines.filter_map(|x|x.ok()).filter_map(|x| x.parse::<i64>().ok()).collect()
+        lines
+            .filter_map(|x| x.ok())
+            .filter_map(|x| x.parse::<i64>().ok())
+            .collect()
     } else {
         Vec::new()
     }
