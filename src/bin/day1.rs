452f5bbd600278fd8f63bd2479c362ee6da8d434
btee <bkramarski@gmail.com>
2023-10-12 22:22:13 +0200

    Elegant day1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,9 +1,42 @@
 use std::io;
 
 fn main() {
+    let list = read_ints();
+
+    // musings(&list);
+
+    // The Solution
+    for window in 1..=5 {
+        let mut increased = 0;
+        for (previous, current) in list.iter().zip(list.iter().skip(window)) {
+            if current > previous {
+                increased += 1;
+            }
+        }
+        println!("Window {}, increased {}", window, increased);
+    }
+}
+
+fn read_ints() -> Vec<i64> {
+    if let Ok(lines) = read_lines() {
+        lines
+            .filter_map(|x| x.ok())
+            .filter_map(|x| x.parse::<i64>().ok())
+            .collect()
+    } else {
+        Vec::new()
+    }
+}
+
+fn read_lines() -> io::Result<io::Lines<io::BufReader<io::Stdin>>> {
+    use io::BufRead;
+    Ok(io::BufReader::new(io::stdin()).lines())
+}
+
+#[allow(dead_code)]
+fn musings(list: &Vec<i64>) {
     let mut increase_counter = 0;
     const FIRST_TAKEN: usize = 0;
-    let list = read_ints();
     if let Some(first) = list.get(FIRST_TAKEN) {
         let mut previous = first;
         for current in list.iter().skip(FIRST_TAKEN + 1) {
@@ -50,22 +83,6 @@ fn main() {
     }
     println!("Increase window {}: {}", WINDOW, increase_window_counter);
 
-    let x = list.iter().skip(list.len() + 2).fold(0, |x,y|x+y);
-    println!("{} skipped?", x)
-}
-
-fn read_ints() -> Vec<i64> {
-    if let Ok(lines) = read_lines() {
-        lines
-            .filter_map(|x| x.ok())
-            .filter_map(|x| x.parse::<i64>().ok())
-            .collect()
-    } else {
-        Vec::new()
-    }
-}
-
-fn read_lines() -> io::Result<io::Lines<io::BufReader<io::Stdin>>> {
-    use io::BufRead;
-    Ok(io::BufReader::new(io::stdin()).lines())
+    let x = list.iter().skip(list.len() + 2).fold(0, |x, y| x + y);
+    println!("{} skipped?", x);
 }
