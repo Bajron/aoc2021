3f9fc22f191ffb34af075c94b61a3032f220808b
btee <bkramarski@gmail.com>
2023-10-13 22:29:54 +0200

    Some further refactoring in day2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,27 +1,32 @@
 use aoc2021::input::read_lines;
 
-struct Command(&'static str, i32);
+enum Order {
+    FORWARD,
+    DOWN,
+    UP,
+}
+struct Command(Order, i32);
 fn main() {
     let lines = read_lines();
 
     if let Ok(lines) = lines {
         let commands = lines
             .filter_map(|x| x.ok())
-            .map(|x| {
-                x.split_whitespace()
-                    .map(|x| x.to_string())
-                    .collect::<Vec<String>>()
-            })
-            .filter(|x| x.len() == 2)
-            .map(|x| (x[0].to_owned(), x[1].parse::<i32>()))
-            .filter_map(|x| match x {
-                (s, Ok(i)) => match s.as_str() {
-                    "forward" => Some(Command("forward", i)),
-                    "down" => Some(Command("down", i)),
-                    "up" => Some(Command("up", i)),
+            .filter_map(|x| {
+                let parts = x.split_whitespace().collect::<Vec<&str>>();
+                match parts[..] {
+                    [name, value] => match name {
+                        "forward" => Some((Order::FORWARD, value.parse::<i32>())),
+                        "down" => Some((Order::DOWN, value.parse::<i32>())),
+                        "up" => Some((Order::UP, value.parse::<i32>())),
+                        _ => {eprintln!(" ** Unkown command {}", name); None},
+                    },
                     _ => None,
-                },
-                _ => None,
+                }
+                .and_then(|p| match p {
+                    (command, Ok(value)) => Some(Command(command, value)),
+                    _ => {eprintln!(" ** String parsing error: {:?}", p.1); None},
+                })
             })
             .collect();
 
@@ -34,12 +39,11 @@ fn main() {
 fn interpret1(commands: &Vec<Command>) {
     let mut depth = 0;
     let mut position = 0;
-    for command in commands {
-        match command.0 {
-            "forward" => position += command.1,
-            "down" => depth += command.1,
-            "up" => depth -= command.1,
-            _ => panic!("unkown command"),
+    for Command(order, value) in commands {
+        match order {
+            Order::FORWARD => position += value,
+            Order::DOWN => depth += value,
+            Order::UP => depth -= value,
         }
     }
 
@@ -53,15 +57,14 @@ fn interpret2(commands: &Vec<Command>) {
     let mut depth = 0;
     let mut position = 0;
     let mut aim = 0;
-    for command in commands {
-        match command.0 {
-            "forward" => {
-                position += command.1;
-                depth += aim * command.1
+    for Command(order, value) in commands {
+        match order {
+            Order::FORWARD => {
+                position += value;
+                depth += aim * value
             }
-            "down" => aim += command.1,
-            "up" => aim -= command.1,
-            _ => panic!("unkown command"),
+            Order::DOWN => aim += value,
+            Order::UP => aim -= value,
         }
     }
 
