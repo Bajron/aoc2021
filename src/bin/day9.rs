1d55cc22e76ad6c729b27a8b719e3ee6a09e2cb9
btee <b.kramarski@gmail.com>
2023-10-16 19:50:37 +0200

    Day 9 part 2

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -56,22 +56,51 @@ fn main() {
                 .all(|&(ny, nx)| height_map[y][x] < height_map[ny][nx])
         };
 
-        let low_values = (1..=height)
+        let low_points = (1..=height)
             .flat_map(|y| (1..=width).map(move |x| (y, x)))
             .filter(is_low)
-            .map(|(y, x)| height_map[y][x])
-            .collect::<Vec<i8>>();
+            .map(|(y, x)| (y, x, height_map[y][x]))
+            .collect::<Vec<(usize, usize, i8)>>();
 
-        println!("{low_values:?}");
-        let score = low_values
+        println!("{low_points:?}");
+        let score = low_points
             .iter()
-            .map(|&x| x as i32 + 1)
+            .map(|&(_, _, x)| x as i32 + 1)
             .fold(0, |acc, v| acc + v);
 
-        println!("Score: {score}")
+        println!("Score: {score}");
+
+        let visited: Vec<Vec<bool>> = vec![vec![false; width + 2]; height_map.len()];
+        let mut fh = FloodHelper(visited, &height_map);
+        let mut basin_sizes = low_points
+            .iter()
+            .map(|&(y, x, _)| fh.fill_count(y, x))
+            .collect::<Vec<u32>>();
+
+        basin_sizes.sort();
+        let last_3_product = basin_sizes.iter().rev().take(3).fold(1, |acc, v| acc * v);
+        println!("Basin score: {last_3_product}")
     }
 }
 
 fn neigbours(y: usize, x: usize) -> [(usize, usize); 4] {
     return [(y - 1, x), (y, x - 1), (y, x + 1), (y + 1, x)];
+}
+
+struct FloodHelper<'a>(Vec<Vec<bool>>, &'a Vec<Vec<i8>>);
+
+impl FloodHelper<'_> {
+    fn fill_count(&mut self, y: usize, x: usize) -> u32 {
+        let FloodHelper(ref mut visited, height_map) = self;
+
+        if visited[y][x] || height_map[y][x] >= 9 {
+            return 0;
+        }
+        visited[y][x] = true;
+
+        neigbours(y, x)
+            .iter()
+            .map(|&(y, x)| self.fill_count(y, x))
+            .fold(1, |acc, v| acc + v)
+    }
 }
