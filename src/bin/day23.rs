f185e288747892de6884b345174068b471ca011b
btee <b.kramarski@gmail.com>
2023-10-27 19:57:05 +0200

    Day23, yet requires code modifications for input

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,5 +1,66 @@
 use std::fmt;
 
+// const ROOM_SIZE: usize = 2;
+const ROOM_SIZE: usize = 4;
+
+fn main() {
+    let state = State {
+        corridor: ['.'; 11],
+        /*
+        room_a: ['B', 'A'],
+        room_b: ['C', 'D'],
+        room_c: ['B', 'C'],
+        room_d: ['D', 'A'],
+        // */
+        //*
+        room_a: ['B', 'D', 'D', 'A'],
+        room_b: ['C', 'C', 'B', 'D'],
+        room_c: ['B', 'B', 'A', 'C'],
+        room_d: ['D', 'A', 'C', 'A'],
+        // */
+        cost: 0,
+    };
+
+    let mut minimal_cost = i32::MAX;
+    search(&state, &mut minimal_cost);
+
+    println!("Final minimal cost example {minimal_cost}");
+
+    let state = State {
+        corridor: ['.'; 11],
+        /*
+        room_a: ['A', 'C'],
+        room_b: ['D', 'D'],
+        room_c: ['A', 'B'],
+        room_d: ['C', 'B'],
+        // */
+        //*
+        room_a: ['A', 'D', 'D', 'C'],
+        room_b: ['D', 'C', 'B', 'D'],
+        room_c: ['A', 'B', 'A', 'B'],
+        room_d: ['C', 'A', 'C', 'B'],
+        // */
+        cost: 0,
+    };
+
+    let mut minimal_cost = i32::MAX;
+    search(&state, &mut minimal_cost);
+
+    println!("Final minimal cost mine {minimal_cost}");
+}
+
+const ALLOWED: [usize; 7] = [0, 1, 3, 5, 7, 9, 10];
+
+fn allowed_on_right(c: char) -> usize {
+    match c {
+        'A' => 2,
+        'B' => 3,
+        'C' => 4,
+        'D' => 5,
+        _ => panic!("Bad letter"),
+    }
+}
+
 fn cost(c: char) -> i32 {
     match c {
         'A' => 1,
@@ -20,58 +81,15 @@ fn corridor_index(c: char) -> usize {
     }
 }
 
-fn main() {
-    let state = State {
-        corridor: ['.'; 11],
-        room_a: ['B', 'A'],
-        room_b: ['C', 'D'],
-        room_c: ['B', 'C'],
-        room_d: ['D', 'A'],
-        cost: 0,
-    };
-
-    let mut minimal_cost = i32::MAX;
-    search(&state, &mut minimal_cost);
-
-    println!("Final minimal cost example {minimal_cost}");
-
-    let state = State {
-        corridor: ['.'; 11],
-        room_a: ['A', 'C'],
-        room_b: ['D', 'D'],
-        room_c: ['A', 'B'],
-        room_d: ['C', 'B'],
-        cost: 0,
-    };
-
-    let mut minimal_cost = i32::MAX;
-    search(&state, &mut minimal_cost);
-
-    println!("Final minimal cost mine {minimal_cost}");
-
-}
-
-const ALLOWED: [usize; 7] = [0, 1, 3, 5, 7, 9, 10];
-
-fn allowed_on_right(c: char) -> usize {
-    match c {
-        'A' => 2,
-        'B' => 3,
-        'C' => 4,
-        'D' => 5,
-        _ => panic!("Bad letter"),
-    }
-}
-
 #[derive(Debug, Clone, Copy)]
 struct State {
     // Disallowed indices 2,4,6,8
     corridor: [char; 11],
     // A-room at 2, B:4, C:6, D:8
-    room_a: [char; 2], // Index 0 is next to corridor
-    room_b: [char; 2],
-    room_c: [char; 2],
-    room_d: [char; 2],
+    room_a: [char; ROOM_SIZE], // Index 0 is next to corridor
+    room_b: [char; ROOM_SIZE],
+    room_c: [char; ROOM_SIZE],
+    room_d: [char; ROOM_SIZE],
     cost: i32,
 }
 
@@ -82,25 +100,16 @@ impl fmt::Display for State {
         let c = self.room_c;
         let d = self.room_d;
 
-        write!(
-            f,
-            "{}\n  {} {} {} {}\n  {} {} {} {}    {}",
-            self.corridor.iter().collect::<String>(),
-            a[0],
-            b[0],
-            c[0],
-            d[0],
-            a[1],
-            b[1],
-            c[1],
-            d[1],
-            self.cost
-        )
+        write!(f, "{}", self.corridor.iter().collect::<String>(),)?;
+        for i in 0..ROOM_SIZE {
+            write!(f, "\n  {} {} {} {}", a[i], b[i], c[i], d[i])?;
+        }
+        write!(f, "     {}", self.cost)
     }
 }
 
 impl State {
-    fn room(&self, ch: char) -> &[char; 2] {
+    fn room(&self, ch: char) -> &[char; ROOM_SIZE] {
         match ch {
             'A' => &self.room_a,
             'B' => &self.room_b,
@@ -109,7 +118,7 @@ impl State {
             _ => panic!("bad room"),
         }
     }
-    fn room_mut(&mut self, ch: char) -> &mut [char; 2] {
+    fn room_mut(&mut self, ch: char) -> &mut [char; ROOM_SIZE] {
         match ch {
             'A' => &mut self.room_a,
             'D' => &mut self.room_d,
@@ -120,24 +129,29 @@ impl State {
     }
 
     fn is_done(&self) -> bool {
-        self.room_a == ['A', 'A']
-            && self.room_b == ['B', 'B']
-            && self.room_c == ['C', 'C']
-            && self.room_d == ['D', 'D']
+        self.room_a.iter().all(|&c| c == 'A')
+            && self.room_b.iter().all(|&c| c == 'B')
+            && self.room_c.iter().all(|&c| c == 'C')
+            && self.room_d.iter().all(|&c| c == 'D')
     }
 }
 
-fn can_enter(room: &[char; 2], native: char) -> Option<usize> {
-    match room {
-        ['.', '.'] => Some(1),
-        ['.', x] => {
-            if *x == native {
-                Some(0)
-            } else {
-                None
-            }
-        }
-        _ => None,
+fn can_enter(room: &[char; ROOM_SIZE], native: char) -> Option<usize> {
+    let mut last_empty: i32 = -1;
+    while (last_empty + 1) < room.len() as i32 && room[(last_empty + 1) as usize] == '.' {
+        last_empty += 1;
+    }
+    if last_empty < 0 {
+        return None;
+    }
+    let last_empty = last_empty as usize;
+    if room[last_empty + 1..room.len()]
+        .iter()
+        .all(|&c| c == native)
+    {
+        Some(last_empty)
+    } else {
+        None
     }
 }
 
@@ -148,8 +162,6 @@ fn search(s: &State, minimal_cost: &mut i32) {
         *minimal_cost = s.cost.min(*minimal_cost);
         return;
     }
-
-    // println!("{s}");
 
     for i in 0..s.corridor.len() {
         let ch = s.corridor[i];
@@ -185,17 +197,11 @@ fn search(s: &State, minimal_cost: &mut i32) {
     // let ri = 0; // Index in the room
     // let room_index = 2;
     // let fa = 2; // from allowed i=2
-    search_room(s, minimal_cost, 0, 'A');
-    search_room(s, minimal_cost, 1, 'A');
-
-    search_room(s, minimal_cost, 0, 'B');
-    search_room(s, minimal_cost, 1, 'B');
-
-    search_room(s, minimal_cost, 0, 'C');
-    search_room(s, minimal_cost, 1, 'C');
-
-    search_room(s, minimal_cost, 0, 'D');
-    search_room(s, minimal_cost, 1, 'D');
+    for ch in ['A', 'B', 'C', 'D'] {
+        for i in 0..ROOM_SIZE {
+            search_room(s, minimal_cost, i, ch);
+        }
+    }
 }
 
 fn search_room(s: &State, minimal_cost: &mut i32, ri: usize, native: char) {
@@ -203,20 +209,21 @@ fn search_room(s: &State, minimal_cost: &mut i32, ri: usize, native: char) {
     let room_index = corridor_index(native);
 
     if s_room[ri] != '.' {
-        if ri == 1 && s_room[0] != '.' {
-            return; // cannot move
+        if s_room[0..ri].iter().any(|&c| c != '.') {
+            return; // cannot move out
         }
         let ch = s_room[ri];
+        // Check if we are in correct room
         if room_index == corridor_index(ch) {
-            if ri == 1 {
-                return; // already in best place
+            if s_room[ri..s_room.len()].iter().all(|&c| c == native) {
+                return; // already in best place, all following is correct char
             } else {
-                if s_room[1] == ch {
-                    return; // room complete
-                }
-                if s_room[1] == '.' {
-                    println!(" NOOOOOO ")
-                    // only makes sense to go there
+                if s_room[ri..s_room.len()]
+                    .iter()
+                    .all(|&c| c == native || c == '.')
+                {
+                    panic!(" NOOOOOO ")
+                    // only makes sense to go deeper
                     // should not happen, make sure in going back from corridor
                 }
             }
@@ -232,7 +239,7 @@ fn search_room(s: &State, minimal_cost: &mut i32, ri: usize, native: char) {
                 let mut next = s.clone();
                 next.room_mut(native)[ri] = '.';
                 next.corridor[pos] = ch;
-                next.cost += pos.abs_diff(room_index - (ri + 1)) as i32 * cost(ch);
+                next.cost += (pos.abs_diff(room_index) + 1 + ri) as i32 * cost(ch);
 
                 search(&next, minimal_cost);
             } else {
